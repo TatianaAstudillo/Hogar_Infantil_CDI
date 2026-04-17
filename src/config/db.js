fe1951a859f98b1cdf6554ce72f638b5f54883ba
TatianaAstudillo <catherinastudillo@gmail.com>
2026-04-17 13:05:43 -0500

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,9 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
